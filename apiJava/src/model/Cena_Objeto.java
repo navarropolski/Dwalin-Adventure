ec5d7adf5e196f7616fedfb178c7ffb941fee39a
Lieslei Adriano Navarro <lieslei.navarro@gmail.com>
2024-09-08 19:18:52 -0300

    chore: Cena_Objeto
    Criado a classe Cena_Objeto para que um item possa ser armazenado e utilizado em outra cena.

---
+++ apiJava/src/model/Cena_Objeto.java
@@ -0,0 +1,40 @@
+package model;
+
+public class Cena_Objeto {
+    private Integer idCenaObjeto;
+    private Integer idObjeto;
+    private Integer idCena;
+    private String nome;
+
+    public Integer getIdCenaObjeto() {
+        return idCenaObjeto;
+    }
+
+    public void setIdCenaObjeto(Integer idCenaObjeto) {
+        this.idCenaObjeto = idCenaObjeto;
+    }
+
+    public Integer getIdObjeto() {
+        return idObjeto;
+    }
+
+    public void setIdObjeto(Integer idObjeto) {
+        this.idObjeto = idObjeto;
+    }
+
+    public Integer getIdCena() {
+        return idCena;
+    }
+
+    public void setIdCena(Integer idCena) {
+        this.idCena = idCena;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
