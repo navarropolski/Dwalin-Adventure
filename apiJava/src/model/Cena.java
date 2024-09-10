3677ed8f6863208149fec9d1cb5d911428a62e40
Lieslei Adriano Navarro <161781529+navarropolski@users.noreply.github.com>
2024-09-09 22:39:33 -0300

    chore: Classe Inventory
    Criei a classe inventory para armazenar os itens obtidos pelo jogador, que será aprimorada.

--- apiJava/src/model/Cena.java
+++ apiJava/src/model/Cena.java
@@ -2,7 +2,7 @@ package model;
 
 import java.util.List;
 
-public class Cena {
+public class Cena { 
     private Integer idCena;
     private String descricao;
     private List<Objeto> itens;
---
+++ apiJava/src/model/Inventory.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Inventory {
+    private Integer idObjeto;
+    private Integer idJogador;
+
+    public Integer getIdObjeto() {
+        return idObjeto;
+    }
+
+    public void setIdObjeto(Integer idObjeto) {
+        this.idObjeto = idObjeto;
+    }
+
+    public Integer getIdJogador() {
+        return idJogador;
+    }
+
+    public void setIdJogador(Integer idJogador) {
+        this.idJogador = idJogador;
+    }
+}
