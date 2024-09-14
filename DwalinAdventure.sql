f0cc80a23a10bcdd5747cdb3e4a4beb2116dd148
Lieslei Adriano Navarro <lieslei.navarro@gmail.com>
2024-09-14 20:43:55 -0300

    recriando o banco
    Dropei e recriei meu banco pois o que havia feito não fazia sentido com a lógica que pensei para o jogo.

--- DwalinAdventure.sql
+++ DwalinAdventure.sql
@@ -1,53 +1,35 @@
-create table CENA (
-	ID INT primary key AUTO_INCREMENT,
-	NOME VARCHAR (100),
-	DESCRICAO TEXT 
+/* create database dwalinadventure */
+
+create table scene(
+	id int auto_increment primary key,
+	name varchar(100) not null,
+	description text not null
 );
 
-create table OBJETO (
-	ID INT primary key AUTO_INCREMENT,
-	NOME VARCHAR (100),
-	DESCRICAO TEXT
+create table itens(
+	id int auto_increment primary key,
+	name varchar(100) not null,
+	description text not null,
+	scene_id int,
+	positiveResult_id int,
+	negativeResult_id int,
+	is_in_inventory boolean default false,
+	foreign key (scene_id) references scene(id),
+	foreign key (positiveResult_id) references scene(id),
+	foreign key (negativeResult_id) references scene(id)
 );
 
-create table CENA_OBJETO (
-	ID INT primary key AUTO_INCREMENT,
-	ID_CENA INT,
-	ID_OBJETO INT,
-	USAR BOOLEAN default FALSE,
-	foreign key (ID_CENA) references CENA (ID),
-	foreign key (ID_OBJETO) references OBJETO (ID)
-);
+alter table itens
+add nextScene_id int;
 
-create table INVENTORY (
-	ID INT primary key auto_increment,
-	ID_JOGADOR INT,
-	ID_OBJETO INT,
-	foreign key (ID_OBJETO) references OBJETO(ID)
-);
-
-create table INTERACAO (
-	ID INT primary key auto_increment,
-	ID_CENA INT,
-	ID_OBJETO INT,
-	ACAO VARCHAR(100),
-	RESULTADO_POSITIVO TEXT,
-	RESULTADO_NEGATIVO TEXT,
-	foreign key (ID_CENA) references CENA(ID),
-	foreign key (ID_OBJETO) references OBJETO(ID)
-);
-
-/*insert into CENA VALUES (
-	'1','ENTRANDO NA MONTANHA','DWALIN ENTRA NA MONTANHA CINZENTA'
-); */
-
-update CENA_OBJETO 
-set 1 = '1','MONTANHA CINZENTA','DWALIN SE AVENTURA NA MONTANHA, DESFILADEIROS ÚMIDOS, PINHEIROS SOLITÁRIOS COBERTOS DE NEVE ADORNAM O AMBIENTE'
-WHERE ID_CENA = 1 AND ID_OBJETO = 2;
-
-update CENA set NOME = "Prólogo", descricao = "Dwalin’s Adventure - Desbrave a cidade perdida de Khaz Badûr como Dwalin o anão explorador, situada nas profundezas das Montanhas Cinzentas em sua perigosa jornada para recuperar sua herança de família, a picareta diamantada de Durin. Sozinho na escuridão prossiga com cautela pois não se sabe o que pode habitar a cidade perdida dos anões, utilize a astúcia e ambição de Dwalin e todas as ferramentas que puder encontrar para enfrentar os desafios à frente.
-" where id = 1;
-
-
-
-select * from CENA
+create table actions(
+	id int auto_increment primary key,
+	item_id int,
+	scene_id int,
+	is_sucess boolean,
+	message text not null,
+	nextScene_id int,
+	foreign key(item_id) references items(id),
+	foreign key(scene_id) references scene(id),
+	foreign key(nextScene_id) references scene(id)
+); 
